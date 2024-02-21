66c1fedf90f15a4e4ac2d98cb61ebea949f20db7
etecture-agoncharova <goncharova_ana@ukr.net>
2024-02-21 15:43:44 +0200

    change folders structure

--- src/data/cats.ts
+++
@@ -1,6 +0,0 @@
-export const Cats = [
-  { id: "1", uri: require('../assets/cats/Charlie.jpg') },
-  { id: "2", uri: require('../assets/cats/Clara.jpg') },
-  { id: "3", uri: require('../assets/cats/Cats.jpg') },
-  { id: "4", uri: require('../assets/cats/Milky.jpg') }
-]
